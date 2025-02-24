b1d5e065b3b16bd9585f4e23efe3a61ac3b032bb
MuratYurtseven <esabet123@hotmail.com>
2025-02-24 17:01:46 +0300

    Art App Created

---
+++ ArtApp/ArtApp/ArtAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAppApp.swift
+//  ArtApp
+//
+//  Created by Murat on 24.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ArtAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ArtApp/ArtAppTests/ArtAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ArtAppTests.swift
+//  ArtAppTests
+//
+//  Created by Murat on 24.02.2025.
+//
+
+import Testing
+@testable import ArtApp
+
+struct ArtAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
